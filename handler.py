b07f63ac459bf816ea27c758dbab136564851202
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-04-09 23:12:15 +0900

    first commit

---
+++ handler.py
@@ -0,0 +1,28 @@
+import os
+import urllib.request
+import json
+
+
+def hello(event, context):
+
+    url = 'https://api.line.me/v2/bot/message/broadcast'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + os.environ['ACCESS_TOKEN']
+    }
+
+    payload = {
+        'messages': [
+            {
+                'type': 'text',
+                'text': '宿題やってますか？',
+            }
+        ]
+    }
+
+    req = urllib.request.Request(url=url, payload=json.dumps(payload).encode('utf-8'), method='POST', headers=headers)
+        
+    with urllib.request.urlopen(req) as res:
+
+        print(res.read().decode("utf-8"))
